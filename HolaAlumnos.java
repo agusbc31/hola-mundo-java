8a8087696d7df0eb328a693da1d0c198e806e263
agusbc31 <agusbc31@gmail.com>
2023-10-04 13:09:42 +0200

    Añadiendo hola alumnos

--- HolaAlumnos.java
+++ HolaAlumnos.java
@@ -1,7 +1,10 @@
 public class HolaAlumnos {
  
 	public static void main(String[] args) {		
+
 		System.out.println("Hola alumnos de DAM1!!");
+		System.out.println("Mi nombre es Agustin Barrero Casquero");
+
 	}
  
 }
